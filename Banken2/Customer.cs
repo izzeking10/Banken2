1d9f4d7b976553c2061e1fa36a7fbd238e807291
Mika Pärnänen <mikaparn@lycksele.se>
2019-09-12 10:55:51 +0200

    Add project files.

---
+++ Banken2/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banken2
+{
+    class Customer
+    {
+
+    }
+}
---
+++ Banken2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Banken2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
